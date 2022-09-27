3e42fcd923109d05ab660ffa44dfa8c536c49303
Otty <otty.praveen@gmail.com>
2022-09-27 11:26:22 +0530

    this is my first commit for GITRACTISEAG

---
+++ src/main/java/GITPRACTISE_AG/App.java
@@ -0,0 +1,13 @@
+package GITPRACTISE_AG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/GITPRACTISE_AG/testCase.java
@@ -0,0 +1,14 @@
+package GITPRACTISE_AG;
+
+import org.testng.annotations.Test;
+
+public class testCase {
+	
+	@Test
+	public void WebAutomationTestCase()
+	{
+		System.out.println("this is WebAutoation Test Case");
+	}
+	
+
+}
